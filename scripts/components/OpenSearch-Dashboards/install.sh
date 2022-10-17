c0d814a3370c7519561e29791ac980d89082d642
Martin Wilhelmi <mnin@mnin.de>
2022-10-17 22:11:23 +0200

    Introduce deb package build
    Signed-off-by: Martin Wilhelmi <mnin@mnin.de>

--- scripts/components/OpenSearch-Dashboards/install.sh
+++ scripts/components/OpenSearch-Dashboards/install.sh
@@ -82,8 +82,12 @@ cd $DIR
 if [ "$DISTRIBUTION" = "tar" ]; then
     cp ../../../config/opensearch_dashboards.yml "$OUTPUT/config/"
 
+elif [ "$DISTRIBUTION" = "deb" ]; then
+    cp ../../../config/opensearch_dashboards.yml "$OUTPUT/../etc/opensearch-dashboards/"
+    cp -a ../../../scripts/pkg/service_templates/opensearch-dashboards/* "$OUTPUT/../"
+    cp -a ../../../scripts/pkg/build_templates/opensearch-dashboards/deb/* "$OUTPUT/../"
 elif [ "$DISTRIBUTION" = "rpm" ]; then
     cp ../../../config/opensearch_dashboards.yml "$OUTPUT/../etc/opensearch-dashboards/"
     cp -a ../../../scripts/pkg/service_templates/opensearch-dashboards/* "$OUTPUT/../"
-    cp -a ../../../scripts/pkg/build_templates/opensearch-dashboards/* "$OUTPUT/../"
+    cp -a ../../../scripts/pkg/build_templates/opensearch-dashboards/rpm/* "$OUTPUT/../"
 fi
